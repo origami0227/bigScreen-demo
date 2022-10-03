87c72280b6efb27f7025e7dc56c6a09f39c437ae
origami <1071242996@qq.com>
2022-10-03 13:51:39 +0800

    优化命名

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,8 +3,8 @@ import './home.scss';
 import headerBg from '../images/header.png'
 export const Home = () => {
   return (
-    <div>
-      <div className="x" style={{backgroundImage: `url(${headerBg})`}}></div>
+    <div className="home">
+      <header style={{backgroundImage: `url(${headerBg})`}}></header>
     </div>
   );
 };
