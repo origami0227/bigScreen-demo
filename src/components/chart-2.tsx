9ee9ea663831da808083dfa698385521ea59195d
origami <1071242996@qq.com>
2022-10-04 16:03:51 +0800

    完成chart2

--- src/components/chart-2.tsx
+++ src/components/chart-2.tsx
@@ -1,49 +1,76 @@
-import React, {useEffect, useRef} from "react"
-import * as echarts from "echarts";
-import {px} from "../shared/px";
-import {baseEchartOptions} from "../shared/base-echart-options";
-import {createEchartsOptions} from "../shared/create-echart-options";
+import React, {useEffect, useRef} from 'react';
+import * as echarts from 'echarts';
+import {px} from '../shared/px';
+import {baseEchartOptions} from '../shared/base-echart-options';
+import {createEchartsOptions} from '../shared/create-echarts-options';
 
-export const Chart2 = () =>{
-    const divRef = useRef(null)
+export const Chart2 = () => {
+    const divRef = useRef(null);
     useEffect(() => {
-        let myChart = echarts.init(divRef.current);
+        var myChart = echarts.init(divRef.current);
         myChart.setOption(createEchartsOptions({
-            ...baseEchartOptions,
-            grid: {
-                x: px(100),
-                y: px(40),
-                x2: px(40),
-                y2: px(40),
-            },
             xAxis: {
                 type: 'value',
-                boundaryGap: [0, 0.01]
+                boundaryGap: [0, 0.01],
+                splitLine: {show: false},
+                axisLabel: {show: false}
             },
             yAxis: {
+                axisTick: {show: false},
                 type: 'category',
-                data: ['巴西', '印尼', '美国', '印度', '中国', '世界人口(万)']
+                data: ['城关区公安局', '七里河区公安局', '西固区公安局', '安宁区公安局', '红古区公安局',
+                    '永登县公安局', '皋兰县公安局', '榆中县公安局', '新区公安局'],
+                axisLabel: {
+                    formatter(val) {
+                        return val.replace('公安局', '\n公安局');
+                    }
+                }
             },
             series: [
                 {
                     name: '2011年',
                     type: 'bar',
-                    data: [18203, 23489, 29034, 104970, 131744, 630230]
+                    data: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    itemStyle: {
+                        normal: {
+                            color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+                                offset: 0,
+                                color: '#2034f9'
+                            }, {
+                                offset: 1,
+                                color: '#04a1ff'
+                            }]),
+                        }
+                    }
                 },
                 {
                     name: '2012年',
                     type: 'bar',
-                    data: [19325, 23438, 31000, 121594, 134141, 681807]
+                    data: [2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    itemStyle: {
+                        normal: {
+                            color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+                                offset: 0,
+                                color: '#b92ae8'
+                            }, {
+                                offset: 1,
+                                color: '#6773e7'
+                            }]),
+                        }
+                    }
                 }
             ]
         }));
     }, []);
-    return (
-        <div className="bordered 管辖统计">
-            <h2>案件破获排名</h2>
-            <div ref={divRef} className="chart">
 
+    return (
+        <div className="bordered 破获排名">
+            <h2>案件破获排名</h2>
+            <div ref={divRef} className="chart"/>
+            <div className="legend">
+                <span className="first"/> 破案排名1
+                <span className="second"/> 破案排名2
             </div>
         </div>
-    )
-}
+    );
+};
