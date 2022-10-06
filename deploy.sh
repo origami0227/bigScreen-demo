a24e372ff0fee17c628c10f69f217a00445ee72e
origami <1071242996@qq.com>
2022-10-06 13:30:42 +0800

    部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
 rm -rf dist &&
-yarn build &&
+yarn build --base ./ &&
 cd dist &&
 git init &&
 git add . &&
 git commit -m "update" &&
+git remote add origin git@github.com:origami0227/bigScreen-website-1.git &&
 git branch -M main &&
-git remote add origin git@github.com:origami0227/bigScreen-website.git &&
 git push -f -u origin main &&
 cd ..
-echo https://origami0227.github.io/bigScreen-website/index.html#/
+echo https://origami0227.github.io/bigScreen-website-1/index.html#/
